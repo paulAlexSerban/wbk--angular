ad85b7a01130b0963c8d93215e439d8b624724d8
paulAlexSerban <paul.alex.serban@gmail.com>
2023-07-11 16:32:32 +0300

    experiments with angular structure

--- frontend/angualr-spa/src/app/app.component.ts
+++ frontend/angualr-spa/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+/**
+ * This is the root component of the application.
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angualr-spa';
+  title: string = 'angualr-spa';
+  subheading: string = 'This is a subheading';
 }
