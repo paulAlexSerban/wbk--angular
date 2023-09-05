19f8efaafcf3dd302613af8474eb4ed7b7462247
paulAlexSerban <paul.alex.serban@gmail.com>
2023-09-05 15:05:27 +0300

    added two way binding

--- frontend/welcome-angular/src/app/app.module.ts
+++ frontend/welcome-angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user.component';
@@ -11,7 +11,7 @@ import { UserComponent } from './user.component';
 
 @NgModule({
   declarations: [AppComponent, UserComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent, UserComponent],
 })
--- frontend/welcome-angular/src/app/user.component.ts
+++ frontend/welcome-angular/src/app/user.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: '#app-user',
+  // [value]="name" is a property binding - you bind the value of the name property to the value property of the input element
+  //    - this should be used when you want to set an element property to a non-string data value or when there is no element property to bind (e.g. <div [ngClass]="{bordered: false}">)
+  // (input)="onUserUserInput($event)" is an event binding - you bind the input event of the input element to the onUserUserInput() method
+  // value="{{ name }}" is an interpolation - you interpolate the value of the name property into the value property of the input element
+  // [(ngModel)]="name" is a two-way binding - you bind the name property to the input element and the input element to the name property
   template: `
-  <input type="text" (input)="onUserUserInput($event)">
+  <input type="text" [(ngModel)]="name">
     <h1>Hello {{ name }}</h1>
     <p>I am new to Angular!</p>
   `,
