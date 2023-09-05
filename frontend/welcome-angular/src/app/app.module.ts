d5eecfdad7b6fa2b2172c2cbcb4ef62c4a5087de
paulAlexSerban <paul.alex.serban@gmail.com>
2023-09-05 15:21:31 +0300

    data binding examples

--- frontend/welcome-angular/src/app/app.module.ts
+++ frontend/welcome-angular/src/app/app.module.ts
@@ -4,15 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user.component';
+import { DashboardComponent } from './dashboard.component';
 
 /**
  * This component will hold our entire application
  */
 
 @NgModule({
-  declarations: [AppComponent, UserComponent],
+  declarations: [AppComponent, UserComponent, DashboardComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent, UserComponent],
+  bootstrap: [AppComponent, UserComponent, DashboardComponent],
 })
 export class AppModule {}
---
+++ frontend/welcome-angular/src/app/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '#app-dashboard',
+  templateUrl: './dashboard.component.html',
+})
+export class DashboardComponent {
+  loadState = 'loading';
+  onSwitchState() {
+    this.loadState = 'finished';
+  }
+}
