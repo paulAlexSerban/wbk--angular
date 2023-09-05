7d86bd80cf2d53fc29639804044685fe963a6489
paulAlexSerban <paul.alex.serban@gmail.com>
2023-09-05 16:52:31 +0300

    added third party libs

--- frontend/welcome-angular/src/app/app.component.ts
+++ frontend/welcome-angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { random } from 'lodash';
 
 @Component({
   // defines a CSS selector with the ID app-root that represents the component in a template
@@ -7,13 +8,13 @@ import { Component } from '@angular/core';
   //            - the selector prefix needs to be unique
   selector: '#app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent {
   title = 'welcome-angular';
   rootName = 'newPaul Name root';
   rootItems = ['Apples', 'Bananas', 'Cherries'];
+  number = 0;
 
   onNameChanged(newName: string) {
     this.rootName = newName;
@@ -24,4 +25,7 @@ export class AppComponent {
     console.log(this.rootItems);
   }
 
+  onIncrease() {
+    this.number = random(0, 10);
+  }
 }
