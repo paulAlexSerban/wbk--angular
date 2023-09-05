8f8c65670f5dc4fe1d08c53028ca968f54231499
paulAlexSerban <paul.alex.serban@gmail.com>
2023-09-05 14:41:39 +0300

    added some notes

--- frontend/welcome-angular/src/app/app.component.ts
+++ frontend/welcome-angular/src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  // defines a CSS selector with the ID app-root that represents the component in a template
+  // the common practice is to define a component's selector as an element selector (e.g. <app-root></app-root>) or an attribute selector (e.g. <div app-root></div>)
+  // NOTA BENE: - this selector is used in the index.html file
+  //            - the selector prefix needs to be unique
+  selector: '#app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- frontend/welcome-angular/src/app/app.module.ts
+++ frontend/welcome-angular/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/**
+ * This component will hold our entire application
+ */
+
 @NgModule({
   declarations: [
     AppComponent
